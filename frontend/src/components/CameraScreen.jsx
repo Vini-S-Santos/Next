0e0367aef729f56fe720678cdd27d9e9352fd002
vinicius <vinicius.souza@stattus4.com>
2025-04-30 15:47:41 -0300

    feat(camerascreen.jsx): modify layout
    modify layout to look like prototype in figma

--- frontend/src/components/CameraScreen.jsx
+++ frontend/src/components/CameraScreen.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import Webcam from 'react-webcam';
+import EllipseButtonIcon from '../assets/EllipseButtonIcon.svg';
 
 export default function CameraScreen({ onCapture }) {
   const webcamRef = useRef(null);
@@ -10,15 +11,29 @@ export default function CameraScreen({ onCapture }) {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-800">
-      <Webcam
-        ref={webcamRef}
-        screenshotFormat="image/jpeg"
-        className="w-full max-w-sm rounded"
-      />
-      <button onClick={handleCapture} className="mt-6 px-4 py-2 bg-green-500 text-white rounded">
-        Capturar
+    <div className="flex flex-col justify-between items-center h-screen bg-gradient-to-br from-white to-gray-300">
+
+      <div className="flex flex-grow items-center justify-center">
+        <div className="w-[700px] h-[900px] bg-black rounded overflow-hidden shadow">
+          <Webcam
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            className="w-full h-full object-cover"
+            videoConstraints={{ facingMode: 'user' }}
+          />
+        </div>
+      </div>
+
+
+     <button
+        onClick={handleCapture}
+        className="absolute bottom-[40px] w-[222px] h-[222px] flex items-center justify-center"
+        style={{ left: 'calc(50% - 111px)' }}
+        aria-label="Capturar"
+      >
+        <img src={EllipseButtonIcon} alt="Botão Capturar" className="w-full h-full" />
       </button>
     </div>
   );
 }
+
